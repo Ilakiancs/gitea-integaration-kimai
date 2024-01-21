9502db4216973305ef032f17be87491ad5e5dc0d
ilakiancs <ilakian15@gmail.com>
2024-01-21 14:30:00 +0530

    add connection timeout validation

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -11,6 +11,7 @@ MAX_PAGE_SIZE = 1000
 # Timeouts
 DEFAULT_TIMEOUT = 30
 MAX_TIMEOUT = 300
+CONNECTION_TIMEOUT = 10
 
 # Status codes
 STATUS_SUCCESS = "success"
@@ -24,3 +25,8 @@ def retry_on_failure(func, max_retries=3): pass  # TODO: implement
 import logging; logger = logging.getLogger(__name__)
 def debug_info(): return {"version": "1.3.0", "debug": True}
 MAX_CONCURRENT_REQUESTS = 10
+
+# Connection validation
+def validate_timeout(timeout):
+    """Validate timeout value is within acceptable range"""
+    return CONNECTION_TIMEOUT <= timeout <= MAX_TIMEOUT
