54c7ca575b9654a84fd427df0370f0745796e68d
ilakiancs <ilakian15@gmail.com>
2025-06-02 14:15:00 +0530

    add application constants file

---
+++ src/utils/constants.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Application constants
+"""
+
+# API endpoints
+API_VERSION = "v1"
+DEFAULT_PAGE_SIZE = 50
+MAX_PAGE_SIZE = 1000
+
+# Timeouts
+DEFAULT_TIMEOUT = 30
+MAX_TIMEOUT = 300
+
+# Status codes
+STATUS_SUCCESS = "success"
+STATUS_ERROR = "error"
+STATUS_PENDING = "pending"
+
+# Sync modes
+SYNC_MODE_FULL = "full"
+SYNC_MODE_INCREMENTAL = "incremental"
