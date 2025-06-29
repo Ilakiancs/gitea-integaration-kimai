4d737edd73c3ba1bd47453a211fb7391cd4309de
ilakiancs <ilakian15@gmail.com>
2025-06-29 15:25:00 +0530

    add concurrency limit constant

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -23,3 +23,4 @@ SYNC_MODE_INCREMENTAL = "incremental"
 def retry_on_failure(func, max_retries=3): pass  # TODO: implement
 import logging; logger = logging.getLogger(__name__)
 def debug_info(): return {"version": "1.3.0", "debug": True}
+MAX_CONCURRENT_REQUESTS = 10
