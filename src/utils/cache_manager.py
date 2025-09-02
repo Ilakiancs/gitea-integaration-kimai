a5038afac59f16ba9189bd82bffe347f2a2aaeba
ilakiancs <ilakian15@gmail.com>
2025-09-02 11:20:00 +0530

    add simple cache manager

---
+++ src/utils/cache_manager.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Simple cache manager for API responses
+"""
+
+import time
+import json
+from typing import Dict, Any, Optional
+
+class SimpleCache:
+    """Simple in-memory cache with TTL support."""
+    
+    def __init__(self, default_ttl: int = 300):
+        self.cache: Dict[str, Dict[str, Any]] = {}
+        self.default_ttl = default_ttl
+    
+    def get(self, key: str) -> Optional[Any]:
+        """Get value from cache."""
+        if key in self.cache:
+            entry = self.cache[key]
+            if time.time() < entry['expires']:
+                return entry['value']
+            else:
+                del self.cache[key]
+        return None
+    
+    def set(self, key: str, value: Any, ttl: Optional[int] = None) -> None:
+        """Set value in cache."""
+        if ttl is None:
+            ttl = self.default_ttl
+        
+        self.cache[key] = {
+            'value': value,
+            'expires': time.time() + ttl,
+            'created': time.time()
+        }
+    
+    def delete(self, key: str) -> bool:
+        """Delete key from cache."""
+        if key in self.cache:
+            del self.cache[key]
+            return True
+        return False
+    
+    def clear(self) -> None:
+        """Clear all cache entries."""
+        self.cache.clear()
+    
+    def cleanup_expired(self) -> int:
+        """Remove expired entries."""
+        current_time = time.time()
+        expired_keys = [
+            key for key, entry in self.cache.items()
+            if current_time >= entry['expires']
+        ]
+        
+        for key in expired_keys:
+            del self.cache[key]
+        
+        return len(expired_keys)
